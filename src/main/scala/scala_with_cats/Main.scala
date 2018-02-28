f4ab31853691853becba067de8a40511d1216d6e
John Olmsted <john@olmsted.io>
2018-02-27 16:03:37 -0800

    Defines an Eq instance for Cats

--- src/main/scala/scala_with_cats/Main.scala
+++ src/main/scala/scala_with_cats/Main.scala
@@ -6,9 +6,11 @@ import scala_with_cats.printable.PrintableInstances._
 import scala_with_cats.printable.PrintableSyntax._
 
 object Main extends App {
+  val toby = Cat("Toby", 4, "calico")
+  val ralph = Cat("Ralph", 10, "orange")
+
   println("Testing printing a Cat with Printable:")
-  val kitty = Cat("Toby", 4, "calico")
-  kitty.print
+  ralph.print
 
   println("Testing printing a Cat with Show (from cats):")
   implicit val catShow: Show[Cat] = Show.show { cat =>
@@ -18,6 +20,15 @@ object Main extends App {
 
     s"$name is a $age year-old $color cat."
   }
-  println(kitty.show)
+  println(ralph.show)
+
+  println("Testing Eq with some Cats:")
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
+    (cat1.name === cat2.name) &&
+    (cat1.age === cat2.age) &&
+    (cat1.color === cat2.color)
+  }
+  println(ralph === toby)
+      
 }
 
